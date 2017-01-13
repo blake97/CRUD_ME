59e9fba7f1ddb9751dc54cf98e4c452812ece6a2
Blake <email": null>
2017-01-12 20:11:57 -0500

    user authentication

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -5,10 +5,10 @@ end
 post '/users' do
   @user = User.new(params[:user])
   if @user.save
+    session[:user_id] = user.id
     redirect '/'
   else
     @errors = @user.errors.full_messages
-
     erb :'users/new'
   end
 end
